efcdc8efb8cf180d67bfc27b8fdd1d11bd835209
Pieter Smith <pieter@boesman.nl>
2014-10-22 22:53:56 +0200

    fs/ext2: support compiling out splice
    Compile out splice support from ext2 when the splice-family of syscalls is not
    supported by the system (i.e. CONFIG_SYSCALL_SPLICE is undefined).

--- fs/ext2/file.c
+++ fs/ext2/file.c
@@ -74,8 +74,8 @@ const struct file_operations ext2_file_operations = {
 	.open		= dquot_file_open,
 	.release	= ext2_release_file,
 	.fsync		= ext2_fsync,
-	.splice_read	= generic_file_splice_read,
-	.splice_write	= iter_file_splice_write,
+	SPLICE_READ_INIT(generic_file_splice_read)
+	SPLICE_WRITE_INIT(iter_file_splice_write)
 };
 
 #ifdef CONFIG_EXT2_FS_XIP
