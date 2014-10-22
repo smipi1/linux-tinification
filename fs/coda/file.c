5b854c2896735493b66cb3fa40829ff0cd3d5b4f
Pieter Smith <pieter@boesman.nl>
2014-10-22 22:48:08 +0200

    fs/coda: support compiling out splice
    Compile out splice support from coda when the splice-family of syscalls is not
    supported by the system (i.e. CONFIG_SYSCALL_SPLICE is undefined).

--- fs/coda/file.c
+++ fs/coda/file.c
@@ -42,6 +42,7 @@ coda_file_read(struct file *coda_file, char __user *buf, size_t count, loff_t *p
 	return host_file->f_op->read(host_file, buf, count, ppos);
 }
 
+#ifdef CONFIG_SYSCALL_SPLICE
 static ssize_t
 coda_file_splice_read(struct file *coda_file, loff_t *ppos,
 		      struct pipe_inode_info *pipe, size_t count,
@@ -62,6 +63,7 @@ coda_file_splice_read(struct file *coda_file, loff_t *ppos,
 
 	return splice_read(host_file, ppos, pipe, count, flags);
 }
+#endif /* #ifdef CONFIG_SYSCALL_SPLICE */
 
 static ssize_t
 coda_file_write(struct file *coda_file, const char __user *buf, size_t count, loff_t *ppos)
@@ -237,6 +239,6 @@ const struct file_operations coda_file_operations = {
 	.open		= coda_open,
 	.release	= coda_release,
 	.fsync		= coda_fsync,
-	.splice_read	= coda_file_splice_read,
+	SPLICE_READ_INIT(coda_file_splice_read)
 };
 
