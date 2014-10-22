2c6013d0170df774e37cddfe589e84283e70f6d6
Pieter Smith <pieter@boesman.nl>
2014-10-22 22:56:04 +0200

    fs/ext4: support compiling out splice
    Compile out splice support from ext4 when the splice-family of syscalls is not
    supported by the system (i.e. CONFIG_SYSCALL_SPLICE is undefined).

--- fs/ext4/file.c
+++ fs/ext4/file.c
@@ -595,8 +595,8 @@ const struct file_operations ext4_file_operations = {
 	.open		= ext4_file_open,
 	.release	= ext4_release_file,
 	.fsync		= ext4_sync_file,
-	.splice_read	= generic_file_splice_read,
-	.splice_write	= iter_file_splice_write,
+	SPLICE_READ_INIT(generic_file_splice_read)
+	SPLICE_WRITE_INIT(iter_file_splice_write)
 	.fallocate	= ext4_fallocate,
 };
 
