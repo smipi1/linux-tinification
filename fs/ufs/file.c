8991976509d3259e9128c24ee95c12033873a6f1
Pieter Smith <pieter@boesman.nl>
2014-10-22 23:16:21 +0200

    fs/ufs: support compiling out splice
    Compile out splice support from ufs when the splice-family of syscalls is not
    supported by the system (i.e. CONFIG_SYSCALL_SPLICE is undefined).

--- fs/ufs/file.c
+++ fs/ufs/file.c
@@ -42,5 +42,5 @@ const struct file_operations ufs_file_operations = {
 	.mmap		= generic_file_mmap,
 	.open           = generic_file_open,
 	.fsync		= generic_file_fsync,
-	.splice_read	= generic_file_splice_read,
+	SPLICE_READ_INIT(generic_file_splice_read)
 };
